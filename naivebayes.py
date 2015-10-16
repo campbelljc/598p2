a3dad6365bac50c132b503382eb6215ecd1fa9da
JCC <jonathan@campbelljc.com>
2015-10-16 15:54:25 -0400

    naive bayes

--- naivebayes.py
+++ naivebayes.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from sklearn.naive_bayes import GaussianNB
 
 # ref : http://stackoverflow.com/questions/8955448/save-load-scipy-sparse-csr-matrix-in-portable-data-format
 
@@ -14,3 +15,10 @@ word_count_matrix = p_load('words.dat')
 # each row is an interview excerpt
 # each column is a word
 # each cell represents the number of times a word occurs in an interview
+
+data = word_count_matrix[:][0:2000]
+target = word_count_matrix[:][-1]
+
+gnb = GaussianNB()
+y_pred = gnb.fit(data, target).predict(data)
+print("Number of mislabeled points out of a total %d points : %d", (iris.data.shape[0],(iris.target != y_pred).sum()))
